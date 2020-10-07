e888c1c492be8451b6b659cc417c4027460007e4
mastergap1 <dakuis123@gmail.com>
2020-10-07 18:15:08 +0700

    music

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <musics />
+  </div>
+</template>
+
+<script>
+import musics from "./components/musics.vue";
+
+export default {
+  name: "App",
+  components: {
+    musics,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  background-image: url("../src/assets/33270.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  margin-top: 50px;
+}
+</style>
